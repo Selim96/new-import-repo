d96852a218593bdb465e9ac9841030107da7bcdb
Selim Fazylov <selim.fazylov1@gmail.com>
2022-05-02 13:45:12 +0300

    add reducer

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, } from "react";
+import React, { /*useState,*/ useEffect, useReducer} from "react";
 import ImagesApi from '../Api/Api';
 import s from './ImageGallery.module.css';
 import { toast } from 'react-toastify';
@@ -18,13 +18,66 @@ const Status = {
     REJECTED: 'rejected',
 }
 
+const State = {
+    images: [],
+    totalHits: '',
+    status: Status.IDLE,
+    showModal: false,
+    imageURL: '',
+    loaderOn: false,
+}
+
+function reduceredState(state, action) {
+    switch (action.type) {
+        case 'addImages':
+            return { ...state, images: action.payload };
+        
+        case 'pushImages':
+            return { ...state, images: [...state.images, ...action.payload] };
+        
+        case 'addTotalHits':
+            return { ...state, totalHits: action.payload };
+        
+        case 'statusIDLE':
+            return { ...state, status: Status.IDLE };
+        
+        case 'statusPENDING':
+            return { ...state, status: Status.PENDING };
+        
+        case 'statusRESOLWED':
+            return { ...state, status: Status.RESOLWED };
+        
+        case 'statusREJECTED':
+            return { ...state, status: Status.REJECTED };
+        
+        case 'modal':
+            return { ...state, showModal: !state.showModal };
+        
+        case 'imageURL':
+            return { ...state, imageURL: action.payload };
+        
+        case 'loaderOn':
+            return { ...state, loaderOn: true };
+        
+        case 'loaderOff':
+            return { ...state, loaderOn: false };
+        
+        default:
+            return state;
+    }
+}
+
 function ImageGallery({imageName}) {
-    const [images, setImages] = useState([]);
-    const [totalHits, setTotalHits] = useState('');
-    const [status, setStatus] = useState(Status.IDLE);
-    const [showModal, setShowModal] = useState(false);
-    const [imageURL, setImageURL] = useState('');
-    const [loaderOn, setLoaderOn] = useState(false);
+    // const [images, setImages] = useState([]);
+    // const [totalHits, setTotalHits] = useState('');
+    // const [status, setStatus] = useState(Status.IDLE);
+    // const [showModal, setShowModal] = useState(false);
+    // const [imageURL, setImageURL] = useState('');
+    // const [loaderOn, setLoaderOn] = useState(false);
+
+    const [state, dispatch] = useReducer(reduceredState, State);
+    
+    const { images, totalHits, status, showModal, imageURL, loaderOn} = state;
     
     api.query = imageName;
 
@@ -33,37 +86,47 @@ function ImageGallery({imageName}) {
             return;
         }
         console.log('RUN UPDATE!!!!!!')
-        setStatus(Status.PENDING);
+        // setStatus(Status.PENDING);
+        dispatch({ type: 'statusPENDING', payload: '',});
     
         api.page = 1;
         api.fetchImages().then(respObj => {
                 if (respObj.hits.length === 0) {
                     return Promise.reject(new Error(`We can't find ${imageName}!`));
                 }
-                setStatus(Status.RESOLWED);
-                setImages(respObj.hits);
-                setTotalHits(respObj.totalHits);
+                // setImages(respObj.hits);
+                dispatch({ type: 'addImages', payload: respObj.hits });
+                // setTotalHits(respObj.totalHits);
+                dispatch({ type: 'addTotalHits', payload: respObj.totalHits });
+                // setStatus(Status.RESOLWED);
+                dispatch({ type: 'statusRESOLWED', payload: '',});
             }).catch(error => {
                 errorFunc(error);
-                setStatus(Status.REJECTED);
+                // setStatus(Status.REJECTED);
+                dispatch({ type: 'statusREJECTED', payload: '',});
             });
     }, [imageName]);
 
     const onLoadMore = () => {
-        setLoaderOn(true);
+        // setLoaderOn(true);
+        dispatch({ type: 'loaderOn', payload: '',});
         api.pageIncrise();
         api.fetchImages().then(respObj => {
-            setImages(prev => [...prev, ...respObj.hits]);
-            setLoaderOn(false);
+            // setImages(prev => [...prev, ...respObj.hits]);
+            dispatch({ type: 'pushImages', payload: respObj.hits });
+            // setLoaderOn(false);
+            dispatch({ type: 'loaderOff', payload: '',});
         });
     }
     
     const modalShowClose = () => {
-        setShowModal(prev => !prev);
+        // setShowModal(prev => !prev);
+        dispatch({ type: 'modal', payload: '',});
     }
     
     const extractUrl = (imageURL) => {
-        setImageURL(imageURL);
+        // setImageURL(imageURL);
+        dispatch({ type: 'imageURL', payload: imageURL,});
     }
     
     function errorFunc(error) {
